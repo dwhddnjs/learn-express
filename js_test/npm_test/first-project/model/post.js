3cfcc5a4f3092284de934edd58250b82202f8007
dwhddnjs <per1215@naver.com>
2022-01-04 01:33:24 +0900

    finished express mogodb

--- js_test/npm_test/first-project/model/post.js
+++ js_test/npm_test/first-project/model/post.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postSchma = new Schema({
-    title: String,
-    content: String,
-    create_date: { type: Date, default: new Date() },
+const postSchema = new Schema({
+  title: String,
+  content: String,
+  create_date: { type: Date, default: new Date() },
 });
 
-module.exports = mongoose.model("post", postSchma);
+module.exports = mongoose.model("post", postSchema);
--- js_test/npm_test/first-project/routes/index.js
+++ js_test/npm_test/first-project/routes/index.js
@@ -1,9 +1,76 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const loginCheck = require('../module/loginCheck')
-const upload = require('../module/imageUpload')
 
+const postModel = require("../model/post");
 
+router.post("/", async (req, res) => {
+  const { title, content } = req.body;
+  const post = new postModel({
+    title: title,
+    content: content,
+  });
+  try {
+    const result = await post.save();
+    res.status(200).json({
+      message: "upload success!!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await postModel.find({});
+    res.status(200).json({
+      message: "read success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await postModel.findById(id);
+    res.status(200).json({
+      message: "detail success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+});
+
+router.put(":/id", async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  try {
+    const result = await postModel.findByIdAndUpdate(id, {
+        title: title,
+        content: content,
+      },{
+        new: true,
+      });
+    res.status(200).json({
+      message: "update success",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err,
+    });
+  }
+});
 
 module.exports = router;
 
@@ -48,4 +115,3 @@ module.exports = router;
 //     result: arr
 //   })
 // })
-
