2aa40b2a887412f12867602d036a64fda978fbaa
dwhddnjs <per1215@naver.com>
2022-01-04 00:28:36 +0900

    add gongoose schma

--- js_test/npm_test/first-project/routes/index.js
+++ js_test/npm_test/first-project/routes/index.js
@@ -4,19 +4,6 @@ const loginCheck = require('../module/loginCheck')
 const upload = require('../module/imageUpload')
 
 
-router.get('/', loginCheck, (req,res)=> {
-  res.status(200).json({
-    message: 'login success!!'
-  })
-})
-
-router.post('/upload', upload.single('image'), (req, res) => {
-  const file = req.file
-  console.log(file)
-  res.status(200).json({
-    message: 'upload success'
-  })
-})
 
 module.exports = router;
 
